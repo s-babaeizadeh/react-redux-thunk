c419f61e4d2388ee95a376e42a185a0c77486d1f
Soudeh Babaeizadeh <babaeizadeh.s@gmail.com>
2023-09-05 23:26:20 +0330

    second version

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { createStore, applyMiddleware } from "redux"
-import { Provider } from "react-redux"
-import "./index.css"
-import thunk from "redux-thunk"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import thunk from 'redux-thunk';
 
-import App from "./App"
-import reducers from "./reducers"
+import App from './App';
+import reducers from './reducers';
 
-const store = createStore(reducers, applyMiddleware(thunk))
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
-)
+  document.getElementById('root')
+);
